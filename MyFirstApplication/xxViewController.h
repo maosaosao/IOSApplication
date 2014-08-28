e4e2e8095fe80a56e74d76811ef698b976152346
xz346 <xin.zhang@yale.edu>
2014-08-28 14:40:07 -0400

    first commit

--- MyFirstApplication/xxViewController.h
+++ MyFirstApplication/xxViewController.h
@@ -9,5 +9,8 @@
 #import <UIKit/UIKit.h>
 
 @interface xxViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UITextField *textField;
+@property (strong, nonatomic) IBOutlet UILabel *titleLabel;
+- (IBAction)buttonPressed:(UIButton *)sender;
 
 @end
--- MyFirstApplication/xxViewController.m
+++ MyFirstApplication/xxViewController.m
@@ -26,4 +26,8 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)buttonPressed:(UIButton *)sender {
+    self.titleLabel.text = self.textField.text;
+    [self.textField resignFirstResponder];
+}
 @end
